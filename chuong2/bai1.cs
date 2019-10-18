78014012660be63eb6fdf530240c566036300fe7
Nguyễn Hoàng Kha <22216580+bkit9x@users.noreply.github.com>
2019-10-18 17:43:36 +0700

    Add files via upload

---
+++ chuong2/bai1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            phanso a, b;
+            a = new phanso();
+            b = new phanso();
+
+            a.nhap();
+            a.toigian();
+            a.tinhtoan();
+
+            b.nhap();
+            b.xuat();
+            if(a>=b)
+                Console.WriteLine("phân số thứ nhất lớn hơn phân số thứ hai");
+            
+            Console.ReadLine();
+        }
+    }
+    class phanso
+    {
+        private int tu, mau;
+        //public phanso()
+        //{
+        //}
+        //public phanso(int tuso, int mauso)
+        //{
+        //    tu = tuso;
+        //    mau = mauso;
+        //}
+        public void nhap()
+        {
+            Console.WriteLine("Nhập tử số: ");
+            tu = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Nhập mẫu số (khác 0): ");
+                mau = Convert.ToInt32(Console.ReadLine());
+            } while (mau == 0);
+        }
+
+        public int UCLN(int a, int b)
+        {
+            if (b == 0) return a;
+            return UCLN(b, a % b);
+        }
+
+        public void toigian()
+        {
+            int ucln = UCLN(tu, mau);
+            tu = tu / ucln;
+            mau = mau / ucln;
+        }
+
+        public void xuat()
+        {
+            //-a, -b : a/b <=> -a/-b 
+            //a, -b: -a/b  <=> -a/-b
+
+            //-a, b : -a/b <=> a/b
+            //a, b : a/b   <=> a/b
+
+            //nếu tử chia hết cho mẫu thì in ra dạng số nguyên
+            if (tu % mau == 0)
+                Console.WriteLine(tu/mau);
+            else if ((tu < 0 && mau < 0) || (tu >= 0 && mau < 0))
+                Console.WriteLine("{0}/{1}", -tu, -mau);
+            else
+                Console.WriteLine("{0}/{1}", tu, mau);
+        }
+
+        public void tinhtoan()
+        {
+            char pt;
+            do
+            {
+                Console.WriteLine("nhập phép tính: ");
+                // Console.ReadLine()[0] : chỉ lấy kí tự ở vị trí đầu tiên (0), nếu người dùng nhập +-*/ thì chỉ lấy phép toán +
+                pt = Console.ReadLine()[0];
+            } while (pt != '+' && pt != '-' && pt != '*' && pt != '/');
+            phanso q = new phanso();
+            q.nhap();
+            switch (pt)
+            {
+                case '+':
+                    tu = tu * q.mau + q.tu * mau;
+                    mau = mau * q.mau;
+                    break;
+                case '-':
+                    tu = tu * q.mau - q.tu * mau;
+                    mau = mau * q.mau;
+                    break;
+                case '*':
+                    tu *= q.tu;
+                    mau *= q.mau;
+                    break;
+                case '/':
+                    tu *= q.mau;
+                    mau *= q.tu;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("kết quả");
+
+            //tối giản phân số trước khi in ra
+            toigian();
+            xuat();
+        }
+
+        public static phanso operator +(phanso p, phanso q)
+        {
+            phanso kq = new phanso();
+            kq.tu = p.tu * q.mau + p.mau * q.tu;
+            kq.mau = p.mau * q.mau;
+            return kq;
+        }
+        public static phanso operator -(phanso p, phanso q)
+        {
+            phanso kq = new phanso();
+            kq.tu = p.tu * q.mau - p.mau * q.tu;
+            kq.mau = p.mau * q.mau;
+            return kq;
+        }
+        public static phanso operator *(phanso p, phanso q)
+        {
+            phanso kq = new phanso();
+            kq.tu = p.tu * q.tu;
+            kq.mau = p.mau * q.mau;
+            return kq;
+        }
+        public static phanso operator /(phanso p, phanso q)
+        {
+            phanso kq = new phanso();
+            kq.tu = p.tu * q.mau;
+            kq.mau = p.mau * q.tu;
+            return kq;
+        }
+
+        public static bool operator <(phanso p, phanso q)
+        {
+            return p.tu * q.mau < q.tu * p.mau;
+        }
+        public static bool operator >(phanso p, phanso q)
+        {
+            return p.tu * q.mau > q.tu * p.mau;
+        }
+        public static bool operator <=(phanso p, phanso q)
+        {
+            return p.tu * q.mau <= q.tu * p.mau;
+        }
+        public static bool operator >=(phanso p, phanso q)
+        {
+            return p.tu * q.mau >= q.tu * p.mau;
+        }
+        public static bool operator ==(phanso p, phanso q)
+        {
+            return p.tu * q.mau == q.tu * p.mau;
+        }
+        public static bool operator !=(phanso p, phanso q)
+        {
+            return p.tu * q.mau != q.tu * p.mau;
+        }
+    }
+}
---
+++ chuong2/bai3.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class bai3
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int n;
+
+            do
+            {
+                Console.WriteLine("Nhập số lượng sinh viên: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n<0 || n>80);
+            sinhvien[] dssv = new sinhvien[n];
+
+            //nhập
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nNhập thông tin sinh viên thứ {0} : ", i+1);
+                dssv[i] = new sinhvien();
+                dssv[i].nhap();
+            }
+            //in
+            Console.WriteLine("\n======danh sách vừa nhập===\n");
+            for (int i = 0; i < n; i++)
+            {
+                dssv[i].xuat();
+            }
+
+            //tìm số sinh viên nữ
+            Console.WriteLine("\n=========\n");
+            int soSVNu = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (dssv[i].GioiTinh)
+                {
+                    soSVNu++;
+                }
+            }
+            Console.WriteLine("Lớp có {0} sinh viên nữ", soSVNu);
+
+
+            //in sv có điểm TB cao nhất
+            Console.WriteLine("\n=========\n");
+            double Maxdtb = dssv[0].Dtb;
+            for (int i = 1; i < n; i++)
+            {
+                if (dssv[i].Dtb > Maxdtb)
+                {
+                    Maxdtb = dssv[i].Dtb;
+                }
+            }
+            Console.WriteLine("Sinh viên có điểm trung bình cao nhất({0}) : ", Maxdtb);
+            for (int i = 0; i < n; i++)
+            {
+                if (dssv[i].Dtb == Maxdtb)
+                {
+                    Console.WriteLine("Họ tên: {0}", dssv[i].HoTen);
+                }
+            }
+
+
+            //Sắp xếp dssv giảm dần theo điểm TB;
+            Console.WriteLine("\n==== danh sách sinh viên sau khi sắp xếp =====\n");
+            sinhvien tmp = new sinhvien();
+
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j = i; j < n; j++)
+                {
+                    if (dssv[i].Dtb < dssv[j].Dtb)
+                    {
+                        tmp = dssv[i];
+                        dssv[i] = dssv[j];
+                        dssv[j] = tmp;
+                    }
+                }
+            }
+            // in ds đã sắp xếp:
+            for (int i = 0; i < n; i++)
+            {
+                dssv[i].xuat();
+            }
+
+            Console.ReadLine();
+        }
+    }
+    class sinhvien
+    {
+        private string _maSo, _hoTen, _queQuan, _xepLoai;
+        private bool _gioiTinh;
+        private int _namSinh;
+        private double _dtb;
+
+        public string MaSo { get => _maSo; set => _maSo = value; }
+        public string HoTen { get => _hoTen; set => _hoTen = value; }
+        public string QueQuan { get => _queQuan; set => _queQuan = value; }
+        public string XepLoai { get => _xepLoai; set => _xepLoai = value; }
+        public int NamSinh { get => _namSinh; set => _namSinh = value; }
+        public double Dtb { get => _dtb; set => _dtb = value; }
+        public bool GioiTinh { get => _gioiTinh; set => _gioiTinh = value; }
+
+        public void nhap()
+        {
+            do
+            {
+                Console.WriteLine("Nhập mã số: ");
+                MaSo = Console.ReadLine();
+            } while (!checkMaSo());
+
+            do
+            {
+                Console.WriteLine("Nhập họ tên: ");
+                HoTen = Console.ReadLine();
+            } while (!checkHoTen());
+
+            Console.WriteLine("Nhập quê quán: ");
+            QueQuan = Console.ReadLine();
+
+            Console.WriteLine("Nhập giới tính (0 : Nam / 1 : Nữ): ");
+            GioiTinh = Convert.ToInt16(Console.ReadLine()) == 0 ? false : true;
+
+            do
+            {
+                Console.WriteLine("Nhập năm sinh (1985-1995): ");
+                NamSinh = Convert.ToInt32(Console.ReadLine());
+            } while (!checkNamSinh());
+
+            do
+            {
+                Console.WriteLine("Nhập điểm trung bình: ");
+                Dtb = Convert.ToDouble(Console.ReadLine());
+            } while (!checkDTB());
+
+            //xếp loại
+            if (Dtb >= 3.6)
+                XepLoai = "Xuất sắc";
+            else if (Dtb >= 3.2)
+                XepLoai = "Giỏi";
+            else if (Dtb >= 2.5)
+                XepLoai = "Khá";
+            else if (Dtb >= 2)
+                XepLoai = "Trung bình";
+            else
+                XepLoai = "Yếu";
+        }
+        public void xuat()
+        {
+            Console.WriteLine("Mã số: {0}", MaSo);
+            Console.WriteLine("Họ tên: {0}", HoTen);
+            Console.WriteLine("Quê quán: {0}", QueQuan);
+            Console.WriteLine("Giới Tính: {0}", GioiTinh ? "Nữ" : "Nam");
+            Console.WriteLine("Năm sinh: {0}", NamSinh);
+            Console.WriteLine("Điểm trung bình: {0}", Dtb);
+            Console.WriteLine("Xếp loại: {0}", XepLoai);
+
+        }
+        private bool checkMaSo()
+        {
+            if (MaSo.Length == 6)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (!((MaSo[i] >= 'a' && MaSo[i] <= 'z') || (MaSo[i] >= 'A' && MaSo[i] <= 'Z')))
+                    {
+                        return false;
+                    }
+                }
+                for (int i = 3; i < 6; i++)
+                {
+                    if (!(MaSo[i] >= '0' && MaSo[i] <= '9'))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        private bool checkHoTen()
+        {
+            for (int i = 0; i < HoTen.Length; i++)
+            {
+                if (!((HoTen[i] >= 'a' && HoTen[i] <= 'z') || (HoTen[i] >= 'A' && HoTen[i] <= 'Z') || HoTen[i]==' '))
+                {
+                    return false;
+                }
+
+                //nếu kí tự đang xét là khoảng trắng, kí tự sau đó cũng là khoảng trắng thì return false;
+                else if (HoTen[i]== ' ' && HoTen[i+1] == ' ')
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+        private bool checkNamSinh()
+        {
+            if (NamSinh < 1985 || NamSinh > 1995)
+            {
+                return false;
+            }
+            return true;
+        }
+        private bool checkDTB()
+        {
+            if (Dtb < 0 || Dtb > 4)
+                return false;
+            return true;
+        }
+    }
+}
